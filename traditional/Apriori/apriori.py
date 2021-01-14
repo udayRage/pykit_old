5336469554be51c59b0d9ecd4a77e73fd171c565
ravi <raviua138@gmail.com>
2021-01-14 15:44:57 +0900

    0.0.3

--- traditional/Apriori/apriori.py
+++ traditional/Apriori/apriori.py
@@ -1,78 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
+import time
+import os
+import psutil
 
-# In[1]:
 
-
-from itertools import combinations
-
-def Canditalist_to_Frequentlist(a):
+def candidate2FrequentItems(cList):
     dicts = {}
     for j in trans:
-        for i in a:
+        for i in cList:
             if i.issubset(j):
-                dicts[frozenset(i)]=dicts.get(frozenset(i),0)+1
-    dicts = {k: v for k, v in dicts.items() if v >= minsup}
+                dicts[frozenset(i)] = dicts.get(frozenset(i), 0) + 1
+    dicts = {k: v for k, v in dicts.items() if v >= minSup}
     return dicts
 
-def Frequentlist_to_Candidatelistgen(a,length):
+
+def frequent2CandidateItems(fList, length):
     c2 = []
-    for i in a:
-        for j in a:
+    for i in fList:
+        for j in fList:
             k = j.union(i)
-            if len(k)==length:
+            if len(k) == length:
                 if k not in c2:
                     c2.append(k)
     return sorted(c2)
 
 
-# In[2]:
+minSup = float(2000)
 
 
-minsup = float(1000)
-
-
-# In[3]:
-
-
-filename = "T10I4D100K.csv"
-
-with open(filename,'r') as f:
-    trans=[]
+filename = "/Users/ravi/PycharmProjects/pami_pykit/Datasets/transactionalDatabase/transactional_T10I4D100K.csv"
+# "T10I4D100K.csv"
+startTime = time.time()
+with open(filename, 'r') as f:
+    trans = []
     for line in f:
-        li=line.split() 
+        li = line.split()
         trans.append(set(li))
     f.close()
 
-itemslist = sorted(list(set.union(*trans)))
+listOfItems = sorted(list(set.union(*trans)))
 items = []
-for i in itemslist:
-    j = set([i])
+for i in listOfItems:
+    j = {i}  # set([i])
     items.append(j)
-itrs = len(items)
-fsets = {}
+noOfItems = len(items)
+finalFps = {}
 
-
-# In[ ]:
-
-
-#Apriori
-#print("\nCandidate Items of Length: 1\n-----------------------------")
-#for ci in items:
-    #print(list(ci))
-for i in range(1,itrs):
-    l = Canditalist_to_Frequentlist(items)
-    print("\nFrequet Items of length:",i,"\n-----------------------------")
-    if len(l)==0:
-        print ("None")
-    for fi in l:
-        print(sorted(fi)," Support Count = ",l[fi])
-    fsets.update(l)
-    items = Frequentlist_to_Candidatelistgen(l,i+1)
-    if len(items)==0:
+for i in range(1, noOfItems):
+    fList = candidate2FrequentItems(items)
+    print("\nFrequent Items of length:", i, "\n-----------------------------")
+    if len(fList) == 0:
+        print("None")
+    for fi in fList:
+        print(sorted(fi), " Support Count = ", fList[fi])
+    finalFps.update(fList)
+    cList = frequent2CandidateItems(fList, i + 1)
+    if len(cList) == 0:
         print("\n-*-*-*-End of Frequent Item sets-*-*-*-")
         break
-    #print("\nCandidate Items of length:",i+1,"\n-----------------------------")
-    for ci in items:
-        print(sorted(list(ci)))     
-
+    # print("\nCandidate Items of length:",i+1,"\n-----------------------------")
+    for ci in cList:
+        print(sorted(list(ci)))
