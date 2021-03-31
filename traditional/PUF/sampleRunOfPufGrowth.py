37bb27abb91227d4512fe9c6cd25d30afb3c9083
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-03-31 15:02:08 +0530

    Create sampleRunOfPufGrowth.py

---
+++ traditional/PUF/sampleRunOfPufGrowth.py
@@ -0,0 +1,28 @@
+import puf as Ap
+
+apri = Ap.Pufgrowth()
+apri.iFile = "tubeSample"
+apri.minSup = 0.1
+apri.maxPer = 0.02
+
+apri.startMine()
+
+frequentPatterns = apri.getFrequentPatterns()
+
+print("Total number of Frequent Patterns:", len(frequentPatterns))
+
+apri.storePatternsInFile("outFile")
+
+# Df = apri.getPatternInDf()
+
+memUSS = apri.getMemoryUSS()
+
+print("Total Memory in USS:", memUSS)
+
+memRSS = apri.getMemoryRSS()
+
+print("Total Memory in RSS", memRSS)
+
+run = apri.getRuntime()
+
+print("Total ExecutionTime in seconds:", run)
