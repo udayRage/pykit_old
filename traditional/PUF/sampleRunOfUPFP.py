dadaa5d458d1a65b78a2e1145a3e863221e13515
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-03-31 15:06:48 +0530

    Create sampleRunOfUPFP.py
    0.0.1

---
+++ traditional/PUF/sampleRunOfUPFP.py
@@ -0,0 +1,28 @@
+import upfp as Ap
+
+apri = Ap.Upfpgrowth()
+apri.iFile = "tubeSample"
+apri.minSup = 0.001
+apri.maxPer = 5
+
+apri.startMine()
+
+frequentPatterns = apri.getFrequentPatterns()
+
+print("Total number of Frequent Patterns:", len(frequentPatterns))
+
+apri.storePatternsInFile("outFile")
+
+# Df = apri.getPatternInDf()
+
+memUSS = apri.getMemoryUSS()
+
+print("Total Memory in USS:", memUSS)
+
+memRSS = apri.getMemoryRSS()
+
+print("Total Memory in RSS", memRSS)
+
+run = apri.getRuntime()
+
+print("Total ExecutionTime in seconds:", run)
