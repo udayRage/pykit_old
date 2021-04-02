e1872019a97a7ed95e8f37e8fc31145a36681be3
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-04-02 06:54:07 +0530

    Create sampleRunOfPsgrowth.py

---
+++ traditional/psgrowth/sampleRunOfPsgrowth.py
@@ -0,0 +1,28 @@
+import psgrowth as Ap
+
+apri = Ap.Psgrowth()
+apri.iFile = "T10I4D100K.txt"
+apri.minSup = 0.001
+apri.maxPer = 0.02
+
+apri.startMine()
+
+frequentPatterns = apri.getFrequentPatterns()
+
+print("Total number of Frequent Patterns:", len(frequentPatterns))
+
+apri.storePatternsInFile("outFile")
+
+# Df = apri.getPatternInDf()
+
+memUSS = apri.getMemoryUSS()
+
+print("Total Memory in USS:", memUSS)
+
+memRSS = apri.getMemoryRSS()
+
+print("Total Memory in RSS", memRSS)
+
+run = apri.getRuntime()
+
+print("Total ExecutionTime in seconds:", run)
